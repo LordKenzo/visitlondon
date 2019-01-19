22f2186e8ac01c54166dfb1e6ff732c089160f0d
LordKenzo <lordkenzo@outlook.it>
2019-01-19 10:09:57 +0100

    browser-sync added with configuration for gulp@4

--- README.md
+++ README.md
@@ -5,7 +5,13 @@ Web workflow with NPM, SASS, Gulp and More
 ## Installation Step
 
 ```
-npm i -D gulp gulp-sass node-sass
+npm i -D gulp gulp-sass node-sass browser-sync
+```
+
+After your first gulp task, run with:
+
+```
+gulp
 ```
 
 ## Old and new way to write a task
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,49 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass');
+// docs: https://browsersync.io/docs/gulp
+const browserSync = require('browser-sync');
+const server = browserSync.create();
+
+const appPath = {
+  dist: {
+    root: 'app/',
+    css: 'app/dist/css',
+    js: 'app/dist/js',
+  },
+  source: {
+    sassSource: 'src/scss/**/*.scss',
+  },
+};
+
 sass.compiler = require('node-sass');
 
+/* Styles */
 function compileSass() {
-  return src('src/scss/app.scss')
+  console.log('compile Sass');
+  return src(appPath.source.sassSource)
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(dest('app/css'));
+    .pipe(dest(appPath.dist.css))
+    .pipe(browserSync.stream());
 }
 
-exports.compileSass = compileSass;
-exports.default = series(compileSass);
+/* Server */
+function serve(done) {
+  console.log('Start Server but before need Sass');
+  server.init({
+    server: {
+      baseDir: appPath.dist.root,
+    },
+  });
+  done();
+}
+
+/* Watch for change */
+function watchWork() {
+  watch(appPath.source.sassSource, compileSass);
+  watch(appPath.dist.root).on('change', server.reload);
+}
+
+/* Tasks */
+const dev = series(compileSass, serve, watchWork);
+exports.watchWork = watchWork;
+exports.default = dev;
